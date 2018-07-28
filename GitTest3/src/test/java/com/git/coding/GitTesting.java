7eface02701faca2e14e38e95b260b30c0762042
Musabay Technologies <Musabay Technologies@MusabayTechCorp.fios-router.home>
2018-07-28 14:11:34 -0400

    ONM-104 updating the World with world

--- GitTest3/src/test/java/com/git/coding/GitTesting.java
+++ GitTest3/src/test/java/com/git/coding/GitTesting.java
@@ -3,7 +3,7 @@ package com.git.coding;
 public class GitTesting {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Program!!!");
+		System.out.println("Hello world Program!!!");
 
 	}
 
